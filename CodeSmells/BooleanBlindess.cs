d7ac05272887e8ede89872eaaa9fb77ddf78ff5c
Poorna Soysa <Poorna Soysa@POORNA-NB>
2024-09-23 06:53:55 +0530

    Add project files.

---
+++ CodeSmells/BooleanBlindess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSmells;
+internal class BooleanBlindess
+{
+    public void ProcessOrderWithBooleanBlindness(int orderId, bool applyDiscount)
+    {
+        if (applyDiscount)
+        {
+            // Apply discount to the order
+        }
+
+        // Process the order here  
+    }
+
+    public enum DiscountOption
+    {
+        None,
+        ApplyDiscount
+    }
+
+    public void ProcessOrderWithoutBooleanBlindness(int orderId, DiscountOption discountOption)
+    {
+        if (discountOption == DiscountOption.ApplyDiscount)
+        {
+            // Apply discount to the order
+        }
+
+        // Process the order here  
+    }
+
+
+}
---
+++ CodeSmells/Program.cs
@@ -0,0 +1,7 @@
+﻿using CodeSmells;
+
+BooleanBlindess booleanBlindess = new();
+
+booleanBlindess.ProcessOrderWithBooleanBlindness(100,applyDiscount:true);
+
+booleanBlindess.ProcessOrderWithoutBooleanBlindness(100, BooleanBlindess.DiscountOption.ApplyDiscount);
