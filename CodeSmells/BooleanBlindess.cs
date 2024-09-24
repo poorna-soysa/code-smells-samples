064ff588829019eb92ad16b9fa658360b4d614ee
Poorna Soysa <poornapiumal.soysa@gmail.com>
2024-09-24 06:12:11 +0530

    Added Data Clumps examples

--- CodeSmells/BooleanBlindess.cs
+++ CodeSmells/BooleanBlindess.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeSmells;
+﻿namespace CodeSmells;
 internal class BooleanBlindess
 {
     public void ProcessOrderWithBooleanBlindness(int orderId, bool applyDiscount)
---
+++ CodeSmells/DataClumps.cs
@@ -0,0 +1,39 @@
+﻿namespace CodeSmells;
+internal class DataClumps
+{
+    public void WithOutDataClumps()
+    {
+        Order order = new()
+        {
+            Id = 1000,
+            OrderDate = DateTime.Now,
+            Customer = new() { FirstName = "John", SecondName = "Doe" }
+        };
+    }
+
+    public void WithDataClumps()
+    {
+        Order order = new()
+        {
+            Id = 1000,
+            OrderDate = DateTime.Now,
+            //CustomerFirstName = "John",
+            //CustomerSecondName = "Doe"
+        };
+    }
+
+
+    private class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+
+    private class Customer
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+    }
+}
